9e1364d2ba664d2916dca81a667325c2ec31f74a
David Castillo Téllez <39785225+Saladito19@users.noreply.github.com>
2024-05-22 09:49:25 -0500

    UWU 2.0

---
+++ Codigos/Mayo22/Mayo22/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Mayo22
+//
+//  Created by OdioMac on 22/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var listadoNoticias: UITableView!
+    var listaNoticias : [Noticias] = [] //VECTOR VACIO
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+

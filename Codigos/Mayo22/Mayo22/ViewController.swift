d18bbfd0ce52a7adbca381bdeb9a335b9de16daa
David Castillo Téllez <39785225+Saladito19@users.noreply.github.com>
2024-05-23 09:54:48 -0500

    Otro

--- Codigos/Mayo22/Mayo22/ViewController.swift
+++ Codigos/Mayo22/Mayo22/ViewController.swift
@@ -7,16 +7,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var listadoNoticias: UITableView!
-    var listaNoticias : [Noticias] = [] //VECTOR VACIO
+    @IBOutlet weak var tvNoticias: UITableView!
+    var listadoNoticias : [Noticia] = [] //VECTOR VACIO
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchNoticias()
+        tvNoticias.delegate = self
+        tvNoticias.dataSource = self
+            // Do any additional setup after loading the view.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        listadoNoticias.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let articulo = listadoNoticias[indexPath.row]
+        
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaNoticia") as! celdaNoticia
+        
+        celda.lblTitulo.text = articulo.title
+        
+        
+        return celda
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+     
+    func fetchNoticias(){
+        //print("NETWORKING")
+        //1.- Crear la URL
+        let cadenaConexion = "https://newsapi.org/v2/everything?q=microsoft&from=2024-05-01&to=2024-05-24&sortBy=popularity&pageSize=30&language=es&apiKey=1f52f28a0f8f4cef8fc32018f2a7a6b3" //Link de la profe
+            
+        if let url = URL(string: cadenaConexion){
+            //2.- Crear un URL Session (Es quien hara la conexion)
+            let sesion = URLSession(configuration: .default)
+                
+            //3.- Darle un task a la sesion
+            let task = sesion.dataTask(with: url){data, response, error in
+            if error != nil{
+                print(error!)
+                return
+            }
+                    
+            if let datosRegresados = data{
+                //let respuesta = String(data:datosRegresados, encoding: .utf8)
+                //print(respuesta)
+                //Descomponer JSON
+                //Decodificador
+                let decoder = JSONDecoder()
+                do {
+                    let decodificado = try decoder.decode(Noticias.self, from: datosRegresados)
+                    //print(decodificado.articles.count)
+                    //Pasamos articulos al vector
+                    self.listadoNoticias = decodificado.articles
+                    //Actualizar la tabla una vez se regrese al hilo principal
+                    DispatchQueue.main.async {
+                        self.tvNoticias.reloadData()
+                    }
+                }
+                catch{
+                    print(error)
+                }
+            }
+            }
+            //4.- Empezar la tarea
+            task.resume()
+    }
+}
 }
 
